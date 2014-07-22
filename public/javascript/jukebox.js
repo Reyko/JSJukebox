6076a41d649f7d6ef557ab54cb5c4b66c8dbd353
Serafeim Maroulis <seramak@gmail.com>
2014-07-22 23:08:26 +0100

    Jukebox is now 3d

--- public/javascript/jukebox.js
+++ public/javascript/jukebox.js
@@ -5,31 +5,78 @@ $(document).ready(function(){
 });
 
 function popUp(){
- var info = document.getElementById("container");
- info.setAttribute("class","display")
+  var info = document.getElementById("container");
+  info.setAttribute("class","display")
 } 
 
 
 function doSearch() {
   var searchTerm = document.getElementById('search').value;
+
+  $(".track").empty();
+
   // Encode spaces
   searchTerm = searchTerm.replace(" ", "+");
   // Search soundcloud for artists
   SC.get('/tracks', { q: searchTerm, license: 'cc-by-sa' }, function(tracks) {
             
-    var imageCon = document.getElementById("cover");
-
-    while (imageCon.firstChild) {
-      imageCon.removeChild(imageCon.firstChild);
-    }
-
+    var counter=0
     for(track in tracks) {
       // console.log(tracks[track]);
+
       var track = new Track(tracks[track]["artwork_url"],tracks[track]["description"],tracks[track]["title"],tracks[track]["uri"])
 
-      // Album cover
-      var cover = document.createElement("img");
+      // Creation of a track
 
+      var cover = document.createElement("div");
+      cover.setAttribute("class","track");
+      cover.setAttribute("data-title",track.title);
+      cover.setAttribute("data-description",track.desc);
+      cover.setAttribute("data-song",track.soundUrl);
+
+      var x3d = document.createElement("x3d");
+      x3d.setAttribute("width","100%");
+      x3d.setAttribute("height","100%");
+
+      var scene = document.createElement("scene");
+   
+      var viewpoint = document.createElement("viewpoint");
+      viewpoint.setAttribute("position","0 0 5");
+
+      var shape = document.createElement("shape");
+
+      var appearance = document.createElement("appearance");
+
+      var material = document.createElement("material");
+      material.setAttribute("diffuseColor","0.603 0.894 0.909");
+
+      var imageTexture = document.createElement("imageTexture");
+      imageTexture.setAttribute("url",track.imgUrl);
+
+      var box = document.createElement("box");
+ 
+      box.setAttribute("size","3,3,0.05");
+
+      cover.appendChild(x3d);
+      x3d.appendChild(scene);
+      scene.appendChild(viewpoint);
+      scene.appendChild(shape);
+      appearance.appendChild(imageTexture);
+      shape.appendChild(appearance);
+      shape.appendChild(box);
+      appearance.appendChild(material);
+
+      var allTracks = document.getElementById("tracks");
+
+
+      // Limitation of tracks due to webgl issue
+      if (track.imgUrl!=null && counter < 20){
+       allTracks.appendChild(cover);
+      }
+      counter+=1
+
+
+      // Album cover modal
       cover.onclick = function(){
 
         var container = document.createElement("div");
@@ -44,6 +91,7 @@ function doSearch() {
         
         var title = this.getAttribute("data-title");
         var description = this.getAttribute("data-description");
+        var song = this.getAttribute("data-song");
 
 
 
@@ -57,7 +105,7 @@ function doSearch() {
         player.setAttribute("height","166");
         player.setAttribute("scrolling","no");
         player.setAttribute("frameborder","no");
-        player.setAttribute("src","https://w.soundcloud.com/player/?url=" + track.soundUrl + "&&show_artwork=true");
+        player.setAttribute("src","https://w.soundcloud.com/player/?url=" + song + "&&show_artwork=true");
         
 
         trackWindow.appendChild(close);
@@ -74,13 +122,8 @@ function doSearch() {
         document.body.appendChild(container);
       }
 
-      if (track.imgUrl!=null){
-        cover.setAttribute("src",track.imgUrl);
-        cover.dataset.description = track.desc;
-        cover.dataset.title = track.title;
-        imageCon.appendChild(cover);
-      }
-        // Album more info
     }
+
+       x3dom.reload();
   });
 };
